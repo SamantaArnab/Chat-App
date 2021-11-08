1dc81b8f764afbbe9ae01241e56415b766e70c3a
arnabsamanta <arnab.samanta@team.telstra.com>
2021-11-08 11:13:24 +0530

    commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,76 @@
-const socket = io()
+const socket = io();
 
-socket.on('message', (message) => {
-    console.log(message)
+const chatForm = document.getElementById("message-form")
+const messageInput = document.getElementById("message-input")
+const messageContainer = document.getElementById('message-container')
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit("new-user", name)
+
+socket.on("message", message => {
+  console.log(message);
+});
+socket.on("chat-message", data => {
+  appendMessage(`${data.name} : ${data.message}`)
 })
 
-document.querySelector("#chat-form").addEventListener('submit', (e) =>{
-    e.preventDefault()
-    const sentMessage = e.target.elements.message.value
-    socket.emit('sentMessage', sentMessage, (deliveryMessage) => {
-        console.log(deliveryMessage)
-    })
+socket.on("user-connected", name => {
+  appendMessage(`${name} connected`)
 })
-document.querySelector('#location-button').addEventListener('click',() => {
-    if(!navigator.geolocation) {
-        return alert('Geolocation is not supported')
-    }
-    navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sentLocation',{latitude: position.coords.latitude, longitude: position.coords.longitude})
-    })
+
+socket.on("user-disconnected", name => {
+  appendMessage(`${name} disconnected`)
 })
+
+chatForm.addEventListener("submit", e => {
+  e.preventDefault();
+  // const sentMessage = e.target.elements.message.value;
+  const message = messageInput.value
+  sentDate = new Date()
+  hour = sentDate.getHours()
+  minute = sentDate.getMinutes()
+  appendMessage(`You: ${message}`, `${hour}:${minute}`)
+  // appendTime(`${hour} : ${minute}`)
+  socket.emit("send-chat-message", message, deliveryMessage => {
+    console.log(deliveryMessage);
+  });
+  messageInput.value = ''
+});
+//-------------
+document.querySelector("#location-button").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported");
+  }
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit(
+      "sentLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      (deliveryMessage) => {
+        console.log(deliveryMessage);
+      }
+    );
+  });
+});
+
+function appendMessage(message, sentTime) {
+  const testContainer = document.createElement('div')
+  testContainer.id = "test";
+  const messageContent = document.createElement('div')
+  messageContent.id = "message"
+  const timeContent = document.createElement('div')
+  timeContent.id = "time"
+  messageContent.innerText = message
+  timeContent.innerText = sentTime
+  testContainer.appendChild(messageContent)
+  testContainer.appendChild(timeContent)
+  messageContainer.appendChild(testContainer)
+
+}
+function appendTime(sentTime) {
+  const messageElement = document.createElement('div',{id: "time"})
+  timeDisplay.innerText = sentTime
+  timeDisplay.append(messageElement)
+}
--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
-const path = require('path');
+const path = require("path");
 const http = require("http");
-const express = require('express');
-const socketio = require('socket.io');
-const Filter = require('bad-words');
+const express = require("express");
+const socketIo = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server);
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const port = 3000;
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-io.on('connection', (socket) => {
-    socket.emit('message',"Welcome!")
-    socket.broadcast.emit('message', 'A new User has joined!')
-    socket.on('sentMessage', (message, callback) => {
-        const filter = new Filter()
-        if(filter.isProfane(message)){
-            return callback('Profanity not allowed!')
-        }
-        io.emit('message', message)
-        callback('The message was delivered!')
-    })
-    socket.on('sentLocation', (coords) => {
-        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-    })
-    socket.on('disconnect', () => {
-        io.emit('message','A user has left!')
-    })
-})
+const  users = {}
+
+io.on("connection", socket => {
+  socket.on("new-user",name =>{
+    users[socket.id] = name
+    socket.broadcast.emit("user-connected", name);
+  })
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new User has joined!");
+  socket.on("send-chat-message", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity not allowed!");
+    }
+    // io.emit("chat-message", message);
+    console.log(users)
+    socket.broadcast.emit("chat-message", {message: message, name: users[socket.id]})
+    callback("The message was delivered!");
+  });
+  socket.on("sentLocation", (coords, callback) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+    callback("Location has been shared!");
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id]
+  });
+});
 server.listen(port, () => {
-    console.log(`chat app listening on port ${port}`)
-})
+  console.log(`chat app listening on port ${port}`);
+});
